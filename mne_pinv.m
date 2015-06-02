5843ec434c1e2cc772dc36998d1871b4e15a582d
Praveen Venkatesh <praveenv253@gmail.com>
2015-06-02 19:45:09 -0400

    Add a separate MNE pseudo-inverse implementation.

---
+++ mne_pinv.m
@@ -0,0 +1,112 @@
+%% Load the forward matrices and compute normal components
+
+clear;
+load 'lead_field_492_2145.mat';
+
+% Read data again, since this is also the normal to the surface
+% No need to use Ted's patch-based method here, because we have a simplistic
+% head model
+% normals = dlmread('points-1212.out');
+% normals = normals(:, 3:5);
+
+num_sensors = size(sens.pnt, 1);
+%num_dipoles = size(dipole_grid, 1);
+num_dipoles = size(lead_field.leadfield(lead_field.inside), 2);
+
+%normals = lead_field.cfg.grid.pos / 7.5;
+dipole_grid = lead_field.pos(lead_field.inside, :);
+normals = dipole_grid ./ (sqrt(sum(dipole_grid.^2, 2)) * ones(1, 3));
+
+L = zeros(num_sensors, num_dipoles);
+lf = lead_field.leadfield(lead_field.inside);
+
+% Find the lead-field of a normal dipole
+for i = 1:num_dipoles
+	forward_matrix = lf{i};
+	% Compute H(q)m(q): m(q) here are unit radial sources
+	forward_matrix = forward_matrix * normals(i, :)';
+	L(:, i) = forward_matrix;
+end
+
+% There really shouldn't be any nans, right?
+L(isnan(L)) = 0;
+
+% ----- Normal forward matrix computation complete ----- %
+
+%% Compute MNE inverse solution
+
+% Arrays to hold the point spread function and bias for the reconstruction of a
+% unit dipole at each voxel
+psf = zeros(num_dipoles, 1);   % Size is equal to the number of dipoles
+bias = zeros(num_dipoles, 1);
+
+% iL is the pseudo-inverse of L: the "reconstruction" matrix, which maps sensor
+% values back to source values
+iL = pinv(L);
+
+for i = 1:num_dipoles
+	%disp(i);
+
+	% The sensor values for a unit dipole at the i'th voxel is simply the
+	% normal lead field vector corresponding to the i'th voxel.
+	measurements = L(:, i);
+
+	% Compute the reconstruction
+	reconstruction = iL * measurements;
+
+	% Compute psf and bias values
+	if(max(reconstruction) > 0)
+		reconstruction = reconstruction ./ max(reconstruction);
+		d_vec = dipole_grid - repmat(dipole_grid(i, :), num_dipoles, 1);
+		d = sqrt(sum(d_vec.^2, 2));
+		psf(i) = max(d(reconstruction > exp(-1)));
+		bias(i) = mean(d(reconstruction == 1));
+	else
+		psf(i) = nan;
+		bias(i) = nan;
+	end
+end
+
+% ----- Done with MNE inverse solution ----- %
+
+%% Plot PSF
+
+figure;
+scatter3(dipole_grid(:, 1), dipole_grid(:, 2), dipole_grid(:, 3), ...
+         20, psf, 'filled');
+
+% Title and axis labels
+title('Width of PSF of reconstruction of a unit dipole at each voxel');
+xlabel('x');
+ylabel('y');
+zlabel('z');
+
+% Colorbar
+colormap(jet);
+cb = colorbar;
+
+% Plot sensor positions
+hold on;
+scatter3(sens.pnt(:,1), sens.pnt(:, 2), sens.pnt(: ,3), 5, 'filled', 'k');
+
+%% Plot Bias
+
+figure;
+scatter3(dipole_grid(:, 1), dipole_grid(:, 2), dipole_grid(:, 3), ...
+         20, bias, 'filled');
+
+% Title and axis labels
+title('Bias of midpoint of reconstruction of a unit dipole at each voxel');
+xlabel('x');
+ylabel('y');
+zlabel('z');
+
+% Colorbar
+colormap(jet);
+cb = colorbar;
+
+% Plot sensor positions
+hold on;
+scatter3(sens.pnt(:,1), sens.pnt(:, 2), sens.pnt(: ,3), 5, 'filled', 'k');
+
+% ----- Done with simulation ----- %
