8b3d2601886b7787c0fee5be1bd7d2e65240d7f0
Praveen Venkatesh <praveenv253@gmail.com>
2015-06-22 01:01:49 -0400

    WIP: Fix parameters for prep-partial in call fn.

--- call_prep_partial.m
+++ call_prep_partial.m
@@ -7,5 +7,5 @@ ft_defaults;
 min_dipole_num = 100 * arg + 1;
 max_dipole_num = min(100 * arg + 100, size(dipole_grid, 1));
 
-prep_partial_leadfield();
+prep_partial_leadfield(num_sensors, num_dipoles, min_dipole_num, max_dipole_num);
 quit;
