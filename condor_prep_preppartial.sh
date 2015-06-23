38b5196135077005546196d986449ab57c05d7c0
Praveen Venkatesh <praveenv253@gmail.com>
2015-06-22 22:38:11 -0400

    Separate program-specific code from condor_submit.

---
+++ condor_prep_preppartial.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+## Variables and functions that this file should define
+#
+# Executable's details:
+# - exec_path
+#   Full path to the executable
+#
+# Job details:
+# - task_name
+#   Name of this program
+# - num_jobs
+#   Number of jobs to create. get_args and get_outfile_name should be able to
+#   handle these many job numbers (see below).
+#
+# Division of labour:
+# - get_args()
+#   A function that accepts the job number (1..$num_jobs), and defines a
+#   variable called `args` - the arguments to be passed to the executable for
+#   that job number.
+# - get_outfile_name()
+#   A function that accepts the job number (1..$num_jobs), and defines a
+#   variable called `outfile_name`, containing a comma separated list of the
+#   names of output files that the executable produces for that job number.
+
+## Note about file permissions:
+#
+# The condor_dir and its subdirs should have suitable file permissions, so that
+# they are readable by remote machines.
+#
+# Check UNIX file permissions with `ls -l` and edit them using `chmod`.
+# - The requisite files should at least have `a+r`.
+#
+# Check AFS ACL settings using `fs la` and edit them using `fs sa`.
+# - The directories should at least have `rl` for system:ece.
+# - For instance, recursively set condor_dir to have suitable permissions, use:
+#       find $condor_dir -type d -exec fs sa {} system:ece rl \;
+
+# Condor directory - directory containing code source repository and other
+# necessary repositories (eg. fieldtrip).
+condor_dir="/afs/ece.cmu.edu/usr/$USER/Public/condor"
+
+# Name of this task - used to name the results folder and the log files.
+task_name="prep_leadfield"
+
+# Path to executable
+exec_path="/usr/local/bin/matlab"
+
+# Program arguments and output file names
+num_sensors=252
+num_dipoles=17199
+get_min_max_dipole_nums() {
+	# Convert job number into dipole slice to operate upon
+	let "min_dipole_num = 100 * $1 + 1"
+	let "max_dipole_num = 100 * $1 + 100"
+	let "max_dipole_num = (max_dipole_num < num_dipoles) ? max_dipole_num : num_dipoles"
+}
+get_args() {
+	get_min_max_dipole_nums $1
+	common_args="-nodesktop -nosplash -r"
+	userpath_cmd="userpath('$condor_dir/10k-eeg:$condor_dir/fieldtrip-20150308');"
+	prep_leadfield_cmd="prep_partial_leadfield($num_sensors, $num_dipoles, $min_dipole_num, $max_dipole_num);"
+	quit_cmd="quit;"
+	args="$common_args \"$userpath_cmd $prep_leadfield_cmd $quit_cmd\""
+}
+get_outfile_name() {
+	get_min_max_dipole_nums $1
+	outfile_name="partial_leadfield_${min_dipole_num}_${max_dipole_num}.mat"
+}
+
+# Job details
+num_jobs=172
--- condor_submit_preppartial.sh
+++ condor_submit_preppartial.sh
@@ -1,50 +1,7 @@
 #!/bin/bash
 
-# Note about file permissions:
-# The directories under condor should have suitable file permissions, so that
-# they are readable by remote machines.
-# Check UNIX file permissions with `ls -l` and edit them using `chmod`.
-# - The requisite files should at least have `a+r`.
-# Check AFS ACL settings using `fs la` and edit them using `fs sa`.
-# - The directories should at least have `rl`.
-
-# Condor directory - directory containing code source repository and other
-# necessary repositories (eg. fieldtrip).
-condor_dir="/afs/ece.cmu.edu/usr/$USER/Public/condor"
-
-# Executable's details
-exec_name="matlab"
-exec_suffix=""
-exec_dir="/usr/local/bin"
-exec_path="$exec_dir/${exec_name}${exec_suffix}"
-
-# Program arguments and output file name
-num_sensors=252
-num_dipoles=17199
-get_min_max_dipole_nums() {
-	# Convert job number into dipole slice to operate upon
-	let "min_dipole_num = 100 * $1 + 1"
-	let "max_dipole_num = 100 * $1 + 100"
-	let "max_dipole_num = (max_dipole_num < num_dipoles) ? max_dipole_num : num_dipoles"
-}
-get_args() {
-	get_min_max_dipole_nums $1
-	common_args="-nodesktop -nosplash -r"
-	userpath_cmd="userpath('$condor_dir/10k-eeg:$condor_dir/fieldtrip-20150308');"
-	prep_leadfield_cmd="prep_partial_leadfield($num_sensors, $num_dipoles, $min_dipole_num, $max_dipole_num);"
-	quit_cmd="quit;"
-	args="$common_args \"$userpath_cmd $prep_leadfield_cmd $quit_cmd\""
-}
-get_outfile_name() {
-	get_min_max_dipole_nums $1
-	outfile_name="partial_leadfield_${min_dipole_num}_${max_dipole_num}.mat"
-}
-
-# Job details
-num_jobs=172
-
 # Results directory and directory for stdout, stderr and Condor logs
-results_dir="/tmp/$USER/${exec_name}_$(date '+%Y%m%d_%H%M%S')"
+results_dir="/tmp/$USER/${task_name}_$(date '+%Y%m%d_%H%M%S')"
 logdir="logs"
 echo "Setting up results directory: $results_dir"
 mkdir -p "$results_dir/$logdir"
@@ -62,9 +19,9 @@ Copy_To_Spool = False
 Priority = 0
 Rank = TARGET.Mips
 
-Output = $logdir/${exec_name}_\$(cluster)_\$(process).out
-Error = $logdir/${exec_name}_\$(cluster)_\$(process).err
-Log = $logdir/${exec_name}_\$(cluster)_\$(process).log
+Output = $logdir/${task_name}_\$(cluster)_\$(process).out
+Error = $logdir/${task_name}_\$(cluster)_\$(process).err
+Log = $logdir/${task_name}_\$(cluster)_\$(process).log
 
 InitialDir = $results_dir
 Should_Transfer_Files = YES
